b4910478acfffec3f54e96be8b2c2b87080ea33b
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-05-25 21:51:09 -0600

    utils and routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoute.js');
+const userRoutes = require('./userRoutes.js');
+const blogRoutes = require('./blogRoutes.js');
 
 
 
 router.use('/users', userRoutes);
+router.use('/blogs', blogRoutes);
 
 module.exports = router;
