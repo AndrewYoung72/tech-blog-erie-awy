c2dd785ea89b4622a6a07a7105ff22e88beadbfa
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-05-26 14:56:20 -0600

    package json and views working

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.get("/", (req, res) => {
+  res.render("homePage")
+})
+router.get("/login", (req, res) => {
+  res.render("login")
+})
 
 module.exports = router;
