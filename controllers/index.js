c9731c1d9b7fd1e11ea5267a6f1b2a519be98a7b
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-05-26 17:04:56 -0600

    wow this project is a lot of typing

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 router.get("/", (req, res) => {
-  res.render("homePage")
-})
+  res.render("homePage");
+});
 router.get("/login", (req, res) => {
-  res.render("login")
-})
+  res.render("login");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 module.exports = router;
