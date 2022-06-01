06c3184b51798eea060114dbcef0286f92775f3d
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-06-01 14:32:00 -0600

    handlebars and login/signup work

--- public/js/login.js
+++ public/js/login.js
@@ -13,11 +13,35 @@ const loginFunction = async (event) => {
     });
     //If  valid user then redirect to blog page
     if (response.ok) {
-      document.location.replace("/blog");
+      document.location.replace("/homePage");
     } else {
       alert(response.statusText);
     }
   }
 };
 
+const signupFunction = async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email-signUp").value.trim();
+  const password = document.getElementById("password-signUp").value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/homePage');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+
 document.querySelector(".login-form").addEventListener("submit", loginFunction);
+
+document.querySelector(".signUp-form").addEventListener("submit", signupFunction);
